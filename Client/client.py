7686335402db99d775132c66a953135cfb023307
Praval <pravalchauhan0906@gmail.com>
2022-10-22 23:44:14 +0530

    Multiple files can be shared

--- Client/client.py
+++ Client/client.py
@@ -2,7 +2,7 @@ import socket
 import tqdm
 import os
 BUFFER_SIZE = 4096
-SERVER : str = input("Enter IP: ")
+SERVER: str = input("Enter IP: ")
 PORT = int(input("Enter PORT: "))
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
@@ -13,24 +13,35 @@ client.connect(ADDR)
 
 
 def receive(client):
-    # Code to send files
     received = client.recv(BUFFER_SIZE).decode(FORMAT)
-    filename , filesize= received.split(SEPARATOR)
-    filename = os.path.basename(filename)
-    filesize = int(filesize)
-    progress = tqdm.tqdm(range(filesize), f"Receiving {filename} " , unit ="B", unit_scale=True, unit_divisor=1024)
-    with open(f"./{filename}","wb") as f:
-        while  True:
-            bytes_read = client.recv(BUFFER_SIZE)
-            if not bytes_read:
+    num_of_files = int(received)
+    client.send("SUCCESS".encode(FORMAT))
+
+    for i in range(num_of_files):
+        received = client.recv(BUFFER_SIZE).decode(FORMAT)
+        filename, filesize = received.split(SEPARATOR)
+        filename = os.path.basename(filename)
+        filesize = int(filesize)
+        client.send("SUCCESS".encode(FORMAT))
+        progress = tqdm.tqdm(range(
+            filesize), f"Receiving {filename} ", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(f"./{filename}", "wb") as f:
+            while True:
+                bytes_read = client.recv(BUFFER_SIZE).decode(FORMAT)
                 progress.update(len(bytes_read))
-                f.close()
-                break
-            f.write(bytes_read)
-            progress.update(len(bytes_read))
-    
-    client.close()        
-    
+                if "{SEPARATOR}SENT{SEPARATOR}" in bytes_read:
+                    bytes_read = bytes_read.replace(
+                        "{SEPARATOR}SENT{SEPARATOR}", "")
+                    bytes_read = bytes(bytes_read, FORMAT)
+                    f.write(bytes_read)
+                    client.send("SUCCESS".encode(FORMAT))
+                    f.close()
+                    break
+                bytes_read = bytes(bytes_read, FORMAT)
+                f.write(bytes_read)
+                progress.update(len(bytes_read))
+
+    client.close()
+
+
 receive(client)
-
-
--- server.py
+++ server.py
@@ -7,32 +7,52 @@ import sys
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, 0)
 FORMAT = 'utf-8'
-SEPARATOR=":"
+SEPARATOR = ":"
 BUFFER_SIZE = 4096
-                    
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-def file_path():
+
+def file_paths():
     fn = sys.argv
     fn.pop(0)
-    return fn 
-    
+    return fn
+
+
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected:  ")
-    files=file_path()
-    filepath=files[0]
-    filesize = os.path.getsize(filepath)
-    conn.send(f"{filepath}{SEPARATOR}{filesize}".encode(FORMAT))
-    progress = tqdm.tqdm(range(filesize),f"Sending {filepath}" , unit ="B" , unit_scale=True , unit_divisor=1024)
-    with open(filepath,"rb") as f:
-        while True:
-            bytes_read = f.read(BUFFER_SIZE)
-            if not bytes_read:
+    print(f"[NEW CONNECTION] {addr} connected: ")
+    files = file_paths()
+    num_of_files = len(files)
+    conn.send(f"{num_of_files}".encode(FORMAT))
+    ack = conn.recv(BUFFER_SIZE).decode(FORMAT)
+    if ack != "SUCCESS":
+        print("ERROR")
+        return
+
+    for file in files:
+        filesize = os.path.getsize(file)
+        conn.send(f"{file}{SEPARATOR}{filesize}".encode(FORMAT))
+        ack = conn.recv(BUFFER_SIZE).decode(FORMAT)
+
+        progress = tqdm.tqdm(range(
+            filesize), f"Sending {file}", unit="B", unit_scale=True, unit_divisor=1024)
+
+        with open(file, "rb") as f:
+            while True:
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read:
+                    progress.update(len(bytes_read))
+                    conn.send("{SEPARATOR}SENT{SEPARATOR}".encode(FORMAT))
+                    ack = conn.recv(BUFFER_SIZE).decode(FORMAT)
+                    ##progress.set_description("We finished")
+                    if ack == "SUCCESS":
+                        f.close()
+                        break
+                    else:
+                        print(f"{file} not sent succesfully!!")
+                conn.send(bytes_read)
                 progress.update(len(bytes_read))
-                break
-            conn.send(bytes_read)
-            progress.update(len(bytes_read))
     conn_close(conn)
 
 
@@ -44,12 +64,14 @@ def conn_close(conn):
 def start(server):
     server.listen()
     print(f"[LISTENING] on {server.getsockname()}")
-    while True:
-        conn, addr = server.accept()
-        # thread = threading.Thread(target=handle_client, args=(conn, addr))
-        # thread.start()
-        # print(f"[ACTIVE] {threading.active_count()-1} ")
-        handle_client(conn, addr)
+
+    conn, addr = server.accept()
+    # thread = threading.Thread(target=handle_client, args=(conn, addr))
+    # thread.start()
+    # print(f"[ACTIVE] {threading.active_count()-1} ")
+    handle_client(conn, addr)
+    print("FILE SENT SUCCESSFULLY ")
+    server.close()
 
 
 print("[STARTING] server is starting : ")
